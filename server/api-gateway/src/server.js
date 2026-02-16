583ea1c5db1b353e7e195603049f39aef26919d6
anuj308 <anujkumarsharma2023@gmail.com>
2026-02-16 13:50:19 +0000

    auth middlerware in services backend

--- server/api-gateway/src/server.js
+++ server/api-gateway/src/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const proxy = require('express-http-proxy')
+const authMiddleware = require('./middleware/auth-middleware')
 
 const app = express();
 const PORT = process.env.PORT || 5000; 
@@ -25,11 +26,11 @@ const proxyOptions = {
     }
 }
 
-app.use('/v1/design', proxy(process.env.DESIGN,{
+app.use('/v1/design', authMiddleware ,proxy(process.env.DESIGN,{
     ...proxyOptions,
 }))
 
-app.use('/v1/media', proxy(process.env.UPLOAD,{
+app.use('/v1/media', authMiddleware ,proxy(process.env.UPLOAD,{
     ...proxyOptions,
      parseReqBody : false
 }))
@@ -38,9 +39,10 @@ app.use('/v1/subscription', proxy(process.env.SUBSCRIPTION,{
     ...proxyOptions,
 }))
 
-app.listen(PORT, () =>
-      console.log(`Api Gateway Service running on port ${PORT}`),
-      console.log(`Design Service running on port ${process.env.DESIGN}`),
-      console.log(`Upload Service running on port ${process.env.UPLOAD}`),
-      console.log(`Subcription Service running on port ${process.env.SUBSCRIPTION}`),
-    );
+app.listen(PORT, () => {
+    console.log(`Api Gateway Service running on port ${PORT}`)
+    console.log(`Design Service running on port ${process.env.DESIGN}`)
+    console.log(`Upload Service running on port ${process.env.UPLOAD}`)
+    console.log(`Subcription Service running on port ${process.env.SUBSCRIPTION}`)
+}
+);
